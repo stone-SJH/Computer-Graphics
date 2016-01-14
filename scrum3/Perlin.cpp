484590dd446100958fa96a7958213131235040c8
stone-SJH <wy30123@163.com>
2016-01-14 14:12:19 +0800

    修正一维二维Perlin函数

--- scrum3/Perlin.cpp
+++ scrum3/Perlin.cpp
@@ -5,22 +5,6 @@ void Perlin::init(){
 	Number_Of_Octaves = 3;
 }
 
-float Perlin::Noise1(int x, int y){
-	x = x % 25;
-	y = y % 25;
-	int n = x + y * 57;
-	n = (n << 13) ^ n;
-	return (1.0f - ((n * (n * n * 15731 + 789221) + 1376312589) & 0x7fffffff) / 1073741824.0f);
-}
-
-//¹â»¬ÔëÉù·¢ÉúÆ÷
-float Perlin::SmoothNoise_1(int x, int y){
-	float corners = (Noise1(x - 1, y - 1) + Noise1(x + 1, y - 1) + Noise1(x - 1, y + 1) + Noise1(x + 1, y + 1)) / 16.0f;
-	float sides = (Noise1(x - 1, y) + Noise1(x + 1, y) + Noise1(x, y - 1) + Noise1(x, y + 1)) / 8.0f;
-	float center = Noise1(x, y) / 4.0f;
-	return corners + sides + center;
-}
-
 float Perlin::Cosine_Interpolate(float a, float b, float x){
 	double ft = x * 3.1415927;
 	double f = (1 - cos(ft)) * 0.5f;
@@ -29,7 +13,55 @@ float Perlin::Cosine_Interpolate(float a, float b, float x){
 
 }
 
-//²åÖµÔëÉù·¢ÉúÆ÷
+float Perlin::Noise0(int x){
+	x = (x << 13) ^ x;
+	return (1.0 - ((x * (x * x * 15731 + 789221) + 1376312589) & 0x7fffffff) / 1073741824.0f);
+}
+
+float Perlin::SmoothNoise_0(float x){
+	return Noise0(x) / 2 + Noise0(x - 1) / 4 + Noise0(x + 1) / 4;
+}
+
+float Perlin::InterpolatedNoise_0(float x){
+	int integer_X = int(x);
+	float fractional_X = x - integer_X;
+
+	float v1 = SmoothNoise_0(integer_X);
+	float v2 = SmoothNoise_0(integer_X + 1);
+
+	return Cosine_Interpolate(v1, v2, fractional_X);
+}
+
+float Perlin::PerlinNoise_1D(float x){
+	float total = 0.0f;
+	float p = persistence;
+	int n = Number_Of_Octaves - 1;
+
+	for (int i = 0; i <= n; i++){
+		float frequency = pow((float)2, i);
+		float amplitude = pow(p, i);
+
+		total = total + InterpolatedNoise_0(x * frequency) * amplitude;
+	}
+
+	return total;
+}
+
+float Perlin::Noise1(int x, int y){
+	x = x % 25;
+	y = y % 25;
+	int n = x + y * 57;
+	n = (n << 13) ^ n;
+	return (1.0f - ((n * (n * n * 15731 + 789221) + 1376312589) & 0x7fffffff) / 1073741824.0f);
+}
+
+float Perlin::SmoothNoise_1(int x, int y){
+	float corners = (Noise1(x - 1, y - 1) + Noise1(x + 1, y - 1) + Noise1(x - 1, y + 1) + Noise1(x + 1, y + 1)) / 16.0f;
+	float sides = (Noise1(x - 1, y) + Noise1(x + 1, y) + Noise1(x, y - 1) + Noise1(x, y + 1)) / 8.0f;
+	float center = Noise1(x, y) / 4.0f;
+	return corners + sides + center;
+}
+
 float Perlin::InterpolatedNoise_1(float x, float y){
 
 	int integer_X = int(x);
--- scrum3/Perlin.h
+++ scrum3/Perlin.h
@@ -9,13 +9,22 @@ private:
 	int Number_Of_Octaves;
 
 	void init();
+	//插值函数
+	float Cosine_Interpolate(float a, float b, float x);
+
+	//一维Perlin函数
+	float Noise0(int x);
+	float SmoothNoise_0(float x);
+	float InterpolatedNoise_0(float x);
+
+	//二维Perlin函数
 	float Noise1(int x, int y);
 	float SmoothNoise_1(int x, int y);
-	float Cosine_Interpolate(float a, float b, float x);
 	float InterpolatedNoise_1(float x, float y);
 
 public:
 	Perlin();
+	float PerlinNoise_1D(float x);
 	float PerlinNoise_2D(float x, float y);
 };
 #endif
